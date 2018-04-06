0f367a0ae5e351262dd2a0aa958337a61f884ccc
Ladji <Ladji@DESKTOP-MVU12UR>
2018-04-05 20:36:04 -0600

    PDFsam God Class pdfsam-fx (SelectionTable)

--- pdfsam-fx/src/main/java/org/pdfsam/ui/selection/multiple/SelectionTable.java
+++ pdfsam-fx/src/main/java/org/pdfsam/ui/selection/multiple/SelectionTable.java
@@ -101,7 +101,7 @@ import javafx.stage.Window;
  * 
  */
 public class SelectionTable extends TableView<SelectionTableRowData> implements ModuleOwned, RestorableView {
-
+	private SelectionTableProduct selectionTableProduct = new SelectionTableProduct();
     private static final Logger LOG = LoggerFactory.getLogger(SelectionTable.class);
 
     private static final DataFormat DND_TABLE_SELECTION_MIME_TYPE = new DataFormat(
@@ -110,7 +110,6 @@ public class SelectionTable extends TableView<SelectionTableRowData> implements 
     private String ownerModule = StringUtils.EMPTY;
     private Label placeHolder = new Label(DefaultI18nContext.getInstance().i18n("Drag and drop PDF files here"));
     private PasswordFieldPopup passwordPopup;
-    private Consumer<SelectionChangedEvent> selectionChangedConsumer;
 
     public SelectionTable(String ownerModule, boolean canDuplicateItems, boolean canMove,
             TableColumnProvider<?>... columns) {
@@ -141,133 +140,14 @@ public class SelectionTable extends TableView<SelectionTableRowData> implements 
         passwordPopup = new PasswordFieldPopup(this.ownerModule);
 
         ContextMenu contextMenu = new ContextMenu();
-        initTopSectionContextMenu(contextMenu, Arrays.stream(columns).anyMatch(PageRangesColumn.class::isInstance));
-        initItemsSectionContextMenu(contextMenu, canDuplicateItems, canMove);
-        initBottomSectionContextMenu(contextMenu);
+        selectionTableProduct.initTopSectionContextMenu(contextMenu, Arrays.stream(columns).anyMatch(PageRangesColumn.class::isInstance),ownerModule);
+        selectionTableProduct.initItemsSectionContextMenu(contextMenu, canDuplicateItems, canMove, ownerModule);
+        selectionTableProduct.initBottomSectionContextMenu(contextMenu);
         setContextMenu(contextMenu);
         eventStudio().addAnnotatedListeners(this);
-        eventStudio().add(SelectionChangedEvent.class, e -> selectionChangedConsumer.accept(e), ownerModule);
     }
 
-    private void initTopSectionContextMenu(ContextMenu contextMenu, boolean hasRanges) {
-        MenuItem setDestinationItem = createMenuItem(DefaultI18nContext.getInstance().i18n("Set destination"),
-                MaterialIcon.FLIGHT_LAND);
-        setDestinationItem.setOnAction(e -> eventStudio().broadcast(
-                requestDestination(getSelectionModel().getSelectedItem().descriptor().getFile(), getOwnerModule()),
-                getOwnerModule()));
-        setDestinationItem.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.ALT_DOWN));
-
-        selectionChangedConsumer = e -> setDestinationItem.setDisable(!e.isSingleSelection());
-        contextMenu.getItems().add(setDestinationItem);
-
-        if (hasRanges) {
-            MenuItem setPageRangesItem = createMenuItem(DefaultI18nContext.getInstance().i18n("Set as range for all"),
-                    MaterialIcon.TOC);
-            setPageRangesItem.setOnAction(e -> eventStudio().broadcast(
-                    new SetPageRangesRequest(getSelectionModel().getSelectedItem().pageSelection.get()),
-                    getOwnerModule()));
-            setPageRangesItem.setAccelerator(new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN));
-            selectionChangedConsumer = selectionChangedConsumer
-                    .andThen(e -> setPageRangesItem.setDisable(!e.isSingleSelection()));
-            contextMenu.getItems().add(setPageRangesItem);
-        }
-        contextMenu.getItems().add(new SeparatorMenuItem());
-    }
-
-    private void initItemsSectionContextMenu(ContextMenu contextMenu, boolean canDuplicate, boolean canMove) {
-
-        MenuItem removeSelected = createMenuItem(DefaultI18nContext.getInstance().i18n("Remove"),
-                MaterialDesignIcon.MINUS);
-        removeSelected.setOnAction(e -> eventStudio().broadcast(new RemoveSelectedEvent(), getOwnerModule()));
-        removeSelected.setAccelerator(new KeyCodeCombination(KeyCode.DELETE));
-        contextMenu.getItems().add(removeSelected);
-        selectionChangedConsumer = selectionChangedConsumer
-                .andThen(e -> removeSelected.setDisable(e.isClearSelection()));
-        if (canMove) {
-            MenuItem moveTopSelected = createMenuItem(DefaultI18nContext.getInstance().i18n("Move to Top"),
-                    MaterialDesignIcon.CHEVRON_DOUBLE_UP);
-            moveTopSelected
-                    .setOnAction(e -> eventStudio().broadcast(new MoveSelectedEvent(MoveType.TOP), getOwnerModule()));
-
-            MenuItem moveUpSelected = createMenuItem(DefaultI18nContext.getInstance().i18n("Move Up"),
-                    MaterialDesignIcon.CHEVRON_UP);
-            moveUpSelected
-                    .setOnAction(e -> eventStudio().broadcast(new MoveSelectedEvent(MoveType.UP), getOwnerModule()));
-
-            MenuItem moveDownSelected = createMenuItem(DefaultI18nContext.getInstance().i18n("Move Down"),
-                    MaterialDesignIcon.CHEVRON_DOWN);
-            moveDownSelected
-                    .setOnAction(e -> eventStudio().broadcast(new MoveSelectedEvent(MoveType.DOWN), getOwnerModule()));
-
-            MenuItem moveBottomSelected = createMenuItem(DefaultI18nContext.getInstance().i18n("Move to Bottom"),
-                    MaterialDesignIcon.CHEVRON_DOUBLE_DOWN);
-            moveBottomSelected.setOnAction(
-                    e -> eventStudio().broadcast(new MoveSelectedEvent(MoveType.BOTTOM), getOwnerModule()));
-
-            contextMenu.getItems().addAll(moveTopSelected, moveUpSelected, moveDownSelected, moveBottomSelected);
-
-            moveBottomSelected.setAccelerator(new KeyCodeCombination(KeyCode.END, KeyCombination.ALT_DOWN));
-            moveDownSelected.setAccelerator(new KeyCodeCombination(KeyCode.DOWN, KeyCombination.ALT_DOWN));
-            moveUpSelected.setAccelerator(new KeyCodeCombination(KeyCode.UP, KeyCombination.ALT_DOWN));
-            moveTopSelected.setAccelerator(new KeyCodeCombination(KeyCode.HOME, KeyCombination.ALT_DOWN));
-
-            selectionChangedConsumer = selectionChangedConsumer.andThen(e -> {
-                moveTopSelected.setDisable(!e.canMove(MoveType.TOP));
-                moveUpSelected.setDisable(!e.canMove(MoveType.UP));
-                moveDownSelected.setDisable(!e.canMove(MoveType.DOWN));
-                moveBottomSelected.setDisable(!e.canMove(MoveType.BOTTOM));
-            });
-        }
-        if (canDuplicate) {
-            MenuItem duplicateItem = createMenuItem(DefaultI18nContext.getInstance().i18n("Duplicate"),
-                    MaterialDesignIcon.CONTENT_COPY);
-            duplicateItem.setOnAction(e -> eventStudio().broadcast(new DuplicateSelectedEvent(), getOwnerModule()));
-            duplicateItem.setAccelerator(new KeyCodeCombination(KeyCode.DIGIT2, KeyCombination.ALT_DOWN));
-
-            contextMenu.getItems().add(duplicateItem);
-
-            selectionChangedConsumer = selectionChangedConsumer
-                    .andThen(e -> duplicateItem.setDisable(e.isClearSelection()));
-        }
-    }
-
-    private void initBottomSectionContextMenu(ContextMenu contextMenu) {
-
-        MenuItem infoItem = createMenuItem(DefaultI18nContext.getInstance().i18n("Document properties"),
-                MaterialDesignIcon.INFORMATION_OUTLINE);
-        infoItem.setOnAction(e -> Platform.runLater(() -> eventStudio()
-                .broadcast(new ShowPdfDescriptorRequest(getSelectionModel().getSelectedItem().descriptor()))));
-
-        MenuItem openFileItem = createMenuItem(DefaultI18nContext.getInstance().i18n("Open"),
-                MaterialDesignIcon.FILE_PDF_BOX);
-        openFileItem.setOnAction(e -> eventStudio()
-                .broadcast(new OpenFileRequest(getSelectionModel().getSelectedItem().descriptor().getFile())));
-
-        MenuItem openFolderItem = createMenuItem(DefaultI18nContext.getInstance().i18n("Open Folder"),
-                MaterialDesignIcon.FOLDER_OUTLINE);
-        openFolderItem.setOnAction(e -> eventStudio().broadcast(
-                new OpenFileRequest(getSelectionModel().getSelectedItem().descriptor().getFile().getParentFile())));
-
-        infoItem.setAccelerator(new KeyCodeCombination(KeyCode.P, KeyCombination.ALT_DOWN));
-        openFileItem.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN));
-        openFolderItem.setAccelerator(
-                new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN, KeyCombination.ALT_DOWN));
-
-        contextMenu.getItems().addAll(new SeparatorMenuItem(), infoItem, openFileItem, openFolderItem);
-
-        selectionChangedConsumer = selectionChangedConsumer.andThen(e -> {
-            infoItem.setDisable(!e.isSingleSelection());
-            openFileItem.setDisable(!e.isSingleSelection());
-            openFolderItem.setDisable(!e.isSingleSelection());
-        });
-    }
-
-    private MenuItem createMenuItem(String text, GlyphIcons icon) {
-        MenuItem item = new MenuItem(text);
-        GlyphsDude.setIcon(item, icon, "1.1em");
-        item.setDisable(true);
-        return item;
-    }
+  
 
     private void initDragAndDrop(boolean canMove) {
         setOnDragOver(e -> dragConsume(e, this.onDragOverConsumer()));
